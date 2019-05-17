93a134ebf7621b62e429727834288d33c9bdc47c
Cristian Henzel <cristianhenzel@users.noreply.github.com>
2019-05-17 16:53:03 +0300

    Move statusCmd initialization to init() function Move statusCmd logic to separate function

--- cmd/status.go
+++ cmd/status.go
@@ -56,60 +56,64 @@ func (statuslist *StatusList) print() {
 }
 
 func init() {
+	var statusCmd = &cobra.Command{
+		Use:   "status",
+		Short: "Show status for all repositories",
+		Run: func(cmd *cobra.Command, args []string) {
+			runStatus()
+		},
+	}
+
 	rootCmd.AddCommand(statusCmd)
 }
 
-var statusCmd = &cobra.Command{
-	Use:   "status",
-	Short: "Show status for all repositories",
-	Run: func(cmd *cobra.Command, args []string) {
-		conf := loadConfig()
-		var status StatusList
+func runStatus() {
+	conf := loadConfig()
+	var status StatusList
 
-		for i, repo := range conf.Repos {
-			status.append(repo.Dir)
-			status.info("Checking", conf.Repos)
+	for i, repo := range conf.Repos {
+		status.append(repo.Dir)
+		status.info("Checking", conf.Repos)
 
-			if !pathExists(repo.Dir) {
-				status[i].appendRed("Absent")
-				continue
-			}
-
-			repository, err := git.PlainOpen(repo.Dir)
-			fatalIfError(err)
-
-			head, err := repository.Head()
-			fatalIfError(err)
-
-			workTree, err := repository.Worktree()
-			fatalIfError(err)
-
-			repoStatus, err := workTree.Status()
-			fatalIfError(err)
-
-			if repoStatus.IsClean() {
-				status[i].appendGreen("Clean")
-			} else {
-				status[i].appendRed("Dirty")
-			}
-
-			remote, err := repository.Remote("origin")
-			fatalIfError(err)
-			remoteRef, err := remote.List(&git.ListOptions{})
-			fatalIfError(err)
-
-			for _, r := range remoteRef {
-				if r.Name().String() == "refs/heads/"+repo.Branch {
-					if r.Hash() == head.Hash() {
-						status[i].appendGreen("Fresh")
-					} else {
-						status[i].appendRed("Stale")
-					}
-					break
-				}
-			}
+		if !pathExists(repo.Dir) {
+			status[i].appendRed("Absent")
+			continue
 		}
 
-		status.print()
-	},
+		repository, err := git.PlainOpen(repo.Dir)
+		fatalIfError(err)
+
+		head, err := repository.Head()
+		fatalIfError(err)
+
+		workTree, err := repository.Worktree()
+		fatalIfError(err)
+
+		repoStatus, err := workTree.Status()
+		fatalIfError(err)
+
+		if repoStatus.IsClean() {
+			status[i].appendGreen("Clean")
+		} else {
+			status[i].appendRed("Dirty")
+		}
+
+		remote, err := repository.Remote("origin")
+		fatalIfError(err)
+		remoteRef, err := remote.List(&git.ListOptions{})
+		fatalIfError(err)
+
+		for _, r := range remoteRef {
+			if r.Name().String() == "refs/heads/"+repo.Branch {
+				if r.Hash() == head.Hash() {
+					status[i].appendGreen("Fresh")
+				} else {
+					status[i].appendRed("Stale")
+				}
+				break
+			}
+		}
+	}
+
+	status.print()
 }
