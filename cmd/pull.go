ac7d059f0acc6f769ef68480481bf99423ad0fed
Cristian Henzel <cristianhenzel@users.noreply.github.com>
2019-05-17 16:48:15 +0300

    Move pullCmd initialization to init() function Move pullCmd logic to separate function

--- cmd/pull.go
+++ cmd/pull.go
@@ -6,56 +6,60 @@ import (
 )
 
 func init() {
+	var pullCmd = &cobra.Command{
+		Use:   "pull",
+		Short: "Pull all repositories",
+		Run: func(cmd *cobra.Command, args []string) {
+			runPull()
+		},
+	}
+
 	rootCmd.AddCommand(pullCmd)
 }
 
-var pullCmd = &cobra.Command{
-	Use:   "pull",
-	Short: "Pull all repositories",
-	Run: func(cmd *cobra.Command, args []string) {
-		conf := loadConfig()
-		var status StatusList
+func runPull() {
+	conf := loadConfig()
+	var status StatusList
 
-		for i, repo := range conf.Repos {
-			status.append(repo.Dir)
-			status.info("Pulling", conf.Repos)
-			var repository *git.Repository
-			var err error
+	for i, repo := range conf.Repos {
+		status.append(repo.Dir)
+		status.info("Pulling", conf.Repos)
+		var repository *git.Repository
+		var err error
 
-			if pathExists(repo.Dir) {
-				repository, err = git.PlainOpen(repo.Dir)
-				fatalIfError(err)
+		if pathExists(repo.Dir) {
+			repository, err = git.PlainOpen(repo.Dir)
+			fatalIfError(err)
 
-				workTree, err := repository.Worktree()
-				fatalIfError(err)
+			workTree, err := repository.Worktree()
+			fatalIfError(err)
 
-				err = workTree.Pull(&git.PullOptions{RemoteName: "origin"})
+			err = workTree.Pull(&git.PullOptions{RemoteName: "origin"})
 
-				if err == git.ErrNonFastForwardUpdate {
-					status[i].appendRed("Non-fast-forward update")
-					continue
-				}
-
-				if err != git.NoErrAlreadyUpToDate {
-					fatalIfError(err)
-				}
-			} else {
-				repository, err = git.PlainClone(repo.Dir, false, &git.CloneOptions{URL: repo.Url})
-				fatalIfError(err)
+			if err == git.ErrNonFastForwardUpdate {
+				status[i].appendRed("Non-fast-forward update")
+				continue
 			}
 
-			repoConf, err := repository.Config()
+			if err != git.NoErrAlreadyUpToDate {
+				fatalIfError(err)
+			}
+		} else {
+			repository, err = git.PlainClone(repo.Dir, false, &git.CloneOptions{URL: repo.Url})
 			fatalIfError(err)
-			section := repoConf.Raw.Section("user")
-			section.SetOption("name", conf.Fullname)
-			section.SetOption("email", conf.Email)
-			err = repoConf.Validate()
-			fatalIfError(err)
-			repository.Storer.SetConfig(repoConf)
-
-			status[i].appendGreen("OK")
 		}
 
-		status.print()
-	},
+		repoConf, err := repository.Config()
+		fatalIfError(err)
+		section := repoConf.Raw.Section("user")
+		section.SetOption("name", conf.Fullname)
+		section.SetOption("email", conf.Email)
+		err = repoConf.Validate()
+		fatalIfError(err)
+		repository.Storer.SetConfig(repoConf)
+
+		status[i].appendGreen("OK")
+	}
+
+	status.print()
 }
