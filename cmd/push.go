9b51b616090e94c1988cc1a92956253f4f8974ed
Cristian Henzel <cristianhenzel@users.noreply.github.com>
2019-05-17 16:51:09 +0300

    Move pushCmd initialization to init() function Move pushCmd logic to separate function

--- cmd/push.go
+++ cmd/push.go
@@ -8,42 +8,46 @@ import (
 const nonFastForwardUpdatePush = "non-fast-forward update: refs/heads/master"
 
 func init() {
+	var pushCmd = &cobra.Command{
+		Use:   "push",
+		Short: "Push all repositories",
+		Run: func(cmd *cobra.Command, args []string) {
+			runPush()
+		},
+	}
+
 	rootCmd.AddCommand(pushCmd)
 }
 
-var pushCmd = &cobra.Command{
-	Use:   "push",
-	Short: "Push all repositories",
-	Run: func(cmd *cobra.Command, args []string) {
-		conf := loadConfig()
-		var status StatusList
+func runPush() {
+	conf := loadConfig()
+	var status StatusList
 
-		for i, repo := range conf.Repos {
-			status.append(repo.Dir)
-			status.info("Pushing", conf.Repos)
+	for i, repo := range conf.Repos {
+		status.append(repo.Dir)
+		status.info("Pushing", conf.Repos)
 
-			repository, err := git.PlainOpen(repo.Dir)
-			if err == git.ErrRepositoryNotExists {
-				status[i].appendRed("Absent")
-				continue
-			}
-			fatalIfError(err)
+		repository, err := git.PlainOpen(repo.Dir)
+		if err == git.ErrRepositoryNotExists {
+			status[i].appendRed("Absent")
+			continue
+		}
+		fatalIfError(err)
 
-			err = repository.Push(&git.PushOptions{})
+		err = repository.Push(&git.PushOptions{})
 
-			if err == git.ErrNonFastForwardUpdate ||
-				err.Error() == nonFastForwardUpdatePush {
-				status[i].appendRed("Non-fast-forward update")
-				continue
-			}
-
-			if err != git.NoErrAlreadyUpToDate {
-				fatalIfError(err)
-			}
-
-			status[i].appendGreen("OK")
+		if err == git.ErrNonFastForwardUpdate ||
+			err.Error() == nonFastForwardUpdatePush {
+			status[i].appendRed("Non-fast-forward update")
+			continue
 		}
 
-		status.print()
-	},
+		if err != git.NoErrAlreadyUpToDate {
+			fatalIfError(err)
+		}
+
+		status[i].appendGreen("OK")
+	}
+
+	status.print()
 }
