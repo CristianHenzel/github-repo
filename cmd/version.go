299fd3c654105735148b7f99fb450313da760cdb
Cristian Henzel <cristianhenzel@users.noreply.github.com>
2019-06-03 16:30:15 +0300

    version: Add logic for printing latest version and self updating

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"fmt"
 
+	semver "github.com/blang/semver"
+	selfupdate "github.com/rhysd/go-github-selfupdate/selfupdate"
 	cobra "github.com/spf13/cobra"
 )
 
@@ -14,15 +16,56 @@ var Version string
 // It gets filled automatically at build time
 var BuildDate string
 
+const updateRepo = "CristianHenzel/github-repo"
+
 func init() {
-	var updateCmd = &cobra.Command{
+	var update bool
+	var versionCmd = &cobra.Command{
 		Use:   "version",
 		Short: "Display version information",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("gr version:", Version)
-			fmt.Println("Built at:", BuildDate)
+			if update {
+				selfUpdate()
+			} else {
+				printVersion()
+			}
 		},
 	}
 
-	rootCmd.AddCommand(updateCmd)
+	versionCmd.Flags().BoolVarP(&update, "update", "u", false, "Update application")
+	rootCmd.AddCommand(versionCmd)
+}
+
+func printVersion() {
+	var vSuffix string
+
+	current := semver.MustParse(Version)
+	latest, found, err := selfupdate.DetectLatest(updateRepo)
+	fatalIfError(err)
+
+	if !found || latest.Version.LTE(current) {
+		vSuffix = "(latest)"
+	} else {
+		vSuffix = "(newer version available: " + latest.Version.String() + ")"
+	}
+
+	fmt.Println("gr version:", Version, vSuffix)
+	fmt.Println("Built at:", BuildDate)
+}
+
+func selfUpdate() {
+	current := semver.MustParse(Version)
+	updater, err := selfupdate.NewUpdater(selfupdate.Config{
+		Validator: &selfupdate.SHA2Validator{},
+	})
+	fatalIfError(err)
+
+	latest, err := updater.UpdateSelf(current, updateRepo)
+	fatalIfError(err)
+
+	if latest.Version.LTE(current) {
+		fmt.Println("You are already using the latest version:", Version)
+	} else {
+		fmt.Println("Successfully updated to version", latest.Version)
+	}
 }
